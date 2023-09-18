1ba23013f27c52ed55d858a39be57e8016369692
Dylan Bailey <wearesleepless@gmail.com>
2023-09-18 10:03:38 -0400

    Append comments

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const buttonEl = document.getElementById("publish-btn")
 const textAreaEl = document.getElementById("endorsement-field")
+const ulEl = document.getElementById("endorsements-ul")
 
 buttonEl.addEventListener("click", function() {
-    console.log(textAreaEl.value)
-    clearInput()
+    let textAreaValue = textAreaEl.value
+    appendCommentToList(textAreaValue)
+    clearTextArea()
 })
 
-function clearInput() {
+function clearTextArea() {
     textAreaEl.value = ""
+}
+
+function appendCommentToList(value) {
+    let newEl = document.createElement("li")
+    newEl.textContent = value
+    ulEl.append(newEl)
 }
