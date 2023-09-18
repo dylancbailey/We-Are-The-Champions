068af89e026e35ebb46c86853ce8a610129cd616
Dylan Bailey <wearesleepless@gmail.com>
2023-09-18 10:21:01 -0400

    Sending and retrieving from DB

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue }from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://we-are-the-champions-e135e-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const commentsInDB = ref(database, "comments")
+
 const buttonEl = document.getElementById("publish-btn")
 const textAreaEl = document.getElementById("endorsement-field")
 const ulEl = document.getElementById("endorsements-ul")
+
+onValue(commentsInDB, function(snapshot) {
+    let comments = Object.entries(snapshot.val())
+
+    clearTextArea()
+
+    for (let i = 0; i < comments.length; i++) {
+        let currentComment = comments[i]
+        let commentID = currentComment[0]
+        let commentValue = currentComment[1]
+
+        appendCommentToList(commentValue)
+    }
+})
 
 buttonEl.addEventListener("click", function() {
     let textAreaValue = textAreaEl.value
     appendCommentToList(textAreaValue)
     clearTextArea()
+
+    push(commentsInDB, textAreaValue)
 })
 
 function clearTextArea() {
