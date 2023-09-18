9fe4e3b887ae8ee405673ee9666bc33aa4aa7976
Dylan Bailey <wearesleepless@gmail.com>
2023-09-18 09:39:11 -0400

    Main styling

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const textAreaEl = document.getElementById("endorsement-field")
 
 buttonEl.addEventListener("click", function() {
     console.log(textAreaEl.value)
+    clearInput()
 })
+
+function clearInput() {
+    textAreaEl.value = ""
+}
